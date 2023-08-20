4994ed9e633c452fde88d2650115758f0ed7370a
Mohammadshuvouup <mohammad.ali.shuvo161@gmail.com>
2023-08-20 12:21:57 +0600

    sub menu hendling js

--- js/menubar-item.js
+++ js/menubar-item.js
@@ -1,2 +1,22 @@
 let menu_item = document.querySelectorAll('.menu-bar .menu-list>li');
-console.log(menu-bar)
+
+menu_item.forEach((li)=>{
+    li.addEventListener("click",(event)=>{
+        let parent = event.target;
+        let sub_menu = parent.querySelector(".sub-menu");
+        hideSubMenu();
+        sub_menu.classList.add("show")
+    })
+});
+
+function hideSubMenu(){
+    menu_item.forEach((li)=>{
+       let sub_menu = li.querySelector(".sub-menu");
+       if(sub_menu.classList.contains("show")){
+        sub_menu.classList.remove("show");
+       }
+
+    })
+}
+
+
